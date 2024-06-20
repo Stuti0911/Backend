43d2202f59ab4d82ad38d3435e1fb805b0ca5a4c
stuti <stutigoel22@gmail.com>
2024-06-21 00:53:47 +0530

    app changes done

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose"
 import { DB_NAME } from "../constants.js"
 
-
 const connectDB=  async()=>{
     try {
         const connectionInst=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`) ;
